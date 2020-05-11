4dce7bcf5799da8195d88eb2ed6ed0721205106d
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-11 05:14:03 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,7 +3,7 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
-class EmailAdrdessParser
+class EmailAddressParser
   EMAILPARSER = ["avi@test.com", "are1@test.com"]
   def parse
   
